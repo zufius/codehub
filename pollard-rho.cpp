b290f4c31d8e18e27be5fa73fe778cb23af66c59
zufius <achung0147@gmail.com>
2020-06-24 12:31:50 +0700

    Update pollard-rho.cpp

--- pollard-rho.cpp
+++ pollard-rho.cpp
@@ -1,160 +1,189 @@
-// https://codeforces.com/contest/797/problem/A
-
+// spoj FACT0 & FACT1
 #include <iostream>
-#include <algorithm>
 #include <stdio.h>
+#include <algorithm>
 #include <vector>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stdlib.h>
 #include <time.h>
-#include <iomanip>
-#include <math.h>
-#include <bitset>
-#include <string.h>
+#include <set>
 
 using namespace std;
+using u128 = __uint128_t;
 
-#define taskname "A"
-#define LL long long 
-#define uLL unsigned long long 
-#define FOR(i,l,r) for(int i = l; i <= r; ++i)
-#define REP(i,l,r) for(int i = l; i < r; ++i)
-#define FORD(i,r,l) for(int i = r; i >= l; --i)
-#define REPD(i,r,l) for(int i = r; i > l; --i)
-#define sz(x) (int)((x).size())
-#define all(x) (x).begin(), (x).end()
-#define debug(x) { cerr << #x << " = " << x << endl; }
-#define sci(x) int x; scanf("%d", &x);
-#define scii(x, y) int x, y; scanf("%d %d", &x, &y);
-#define sciii(x, y, z) int x, y, z; scanf("%d %d %d", &x, &y, &z);
-#define pi pair<int,int>
-#define pii pair<int,pi>
+const int N = 5e5 + 123;
+const int oo = 1e9 + 123;
+const u128 mod = 1e9 + 7;
+
+typedef pair<int,int> pi;
 #define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define _USE_MATH_DEFINES 
-/* M_PI */
+#define getSz(x) ((x).size())
 #define SetLength2(a, n, t) a=((t*) calloc(n, sizeof(t))) + (n)/2
 
-const int maxN = 2e5 + 11;
-const LL oo = 1e17 + 11;
+int n,m;
 
-int tcases = 1;
+namespace MillerRabinTest{
+    vector <u128> smallPrime,witness;
 
-int L,R,res = 0;
+    void init(){
 
-class{
-    public : 
-    LL powmod(LL x,LL y,LL n){
-        LL rr = 1;
-        while(y){
-            if(y & 1) rr = rr * x % n;
-            x = x * x % n;
-            y >>= 1;
-        }
-        return rr;
     }
-    pi factor(LL n){
-        int m = 0;
+    pair<u128,u128> factor(u128 n){
+        u128 m = 0;
         while(!(n & 1)){
             ++m;
             n >>= 1;
         }
-        return mp(m,n);
+        return make_pair(m,n);
     }
-
-    bool check(LL n,LL s,LL d,LL a){
-        LL x = powmod(a,d,n);
+    u128 modpower(u128 x,u128 y,u128 n){
+        u128 rr = 1;
+        while(y){
+            if(y & 1) rr = (u128)rr * x % n;
+            x = (u128)x*x % n;
+            y >>= 1;
+        }
+        return rr;
+    }
+    bool check(u128 n,u128 s,u128 d, u128 a){
+        u128 x = modpower(a,d,n);
         if(x == 1) return 1;
-        for(;s; s--){
+        for(;s > 0; --s){
             if(x == n - 1) return 1;
-            x = x * x % n;
+            x = (u128)x * x % n;
         }
         return 0;
     }
-    
-    bool isPrime(LL n){
-        if(n < 5) return n == 2 || n == 3;
-        if(n % 2 == 0 || n % 3 == 0) return 0;
-        pi ft = factor(n - 1);
-        return check(n,ft.fi,ft.se,2) && check(n,ft.fi,ft.se,3);
-    }
-} MillerRabin;
-
-vector <int> factorization;
-vector <int> divisor[1234];
-
-void init(){
-    REP(i,2,1234){
-        int x = i;
-        for(int j = 2; j * j <= x; ++j) if(x % j == 0){
-            while(x % j == 0){
-                divisor[i].pb(j);
-                x /= j;
-            }
+    bool miller(u128 x){
+        for(u128 p : smallPrime){
+            if(x == p) return 1;
+            if(x % p == 0) return 0;
         }
-        if(x > 1) divisor[i].pb(x);
+        pair<u128,u128> ft = factor(x - 1);
+        for(u128 a : witness) if(!check(x,ft.first,ft.second,a)) return 0;
+        return 1;
     }
+};
+
+void refine(vector <pair<u128,int> > &f){
+    sort(f.begin(),f.end());
+    int j = 0;
+    for(int i = 1; i < f.size(); ++i){
+        if(f[i].first == f[j].first){
+            f[j].second += f[i].second;
+        }else{
+            f[++j] = f[i];
+        }
+    }
+    while(f.size() > j + 1) f.pop_back();
 }
 
-LL _next(LL x,int n){
-    return (x * x + 1) % n;
-}
+vector <pair<u128,int> > factors,smallFactors[1234];
 
-LL __gcd(LL x,LL y){
-    return y == 0 ? x : __gcd(y,x % y);
+u128 f(u128 a,u128 c,u128 mod){
+    return ((u128)a * a + c) % mod;
 }
-
-int find_factor(int n){
-    LL x = rand() % n,y = x,p = 1;
-    do{
-        x = _next(x,n);
-        y = _next(_next(y,n),n);
-        p = __gcd(llabs(x - y),n);
-    }while(p == 1);
-    return p;
+u128 gcd(u128 x,u128 y){
+    return y == 0 ? x : gcd(y,x % y);
 }
-void RHO(int n){
-    if(n < 1234){
-        factorization.insert(factorization.end(),divisor[n].begin(),divisor[n].end());
+u128 next(u128 a,u128 x){
+    return ((u128)a * a + 1) % x;
+}
+u128 find_factor(u128 x){
+    u128 a = rand() % x,b = a,times = 0,g = 1;
+    while(g == 1 && times < 3456){
+        ++times;
+        a = next(a,x);
+        b = next(next(b,x),x);
+        g = gcd(llabs(a - b),x);
+    }
+    return g;
+}
+void RHO(u128 x){
+    if(x < 1024){
+        factors.insert(factors.end(),smallFactors[x].begin(),smallFactors[x].end());
         return ;
     }
-    if(MillerRabin.isPrime(n)){
-        factorization.pb(n);
+    if(MillerRabinTest::miller(x)){
+        factors.push_back(mp(x,1));
         return ;
     }
-    int p = 0;
-    while(p == 0 || p == n){
-        p = find_factor(n);
+    u128 d = 1;
+    while(d == 1 || d == x){
+        d = find_factor(x);
     }
-    RHO(p);
-    RHO(n/p);
+    RHO(d);
+    RHO(x/d);
 }
-
-int n,k;
+void factorize(u128 x){
+    factors.clear();
+    RHO(x);
+}
+bool readInt(u128 &x){
+    string s;
+    cin >> s;
+    if(s == "0") return 0;
+    x = 0;
+    for(int i = 0; i < s.size(); ++i){
+        x = x * 10 + (s[i] - '0');
+    }
+    return 1;
+}
+string printInt(u128 x){
+    string s;
+    while(x > 0){
+        s = (char)('0' + x % 10) + s;
+        x /= 10;
+    }
+    return s;
+}
 void testcase(){
-    init();
-    scanf("%d%d",&n,&k);
-    RHO(n);
-    if((int)factorization.size() < k) puts("-1");
-    else{
-        while((int)factorization.size() > k){
-            int x = factorization.back();
-            factorization.pop_back();
-            factorization.back() *= x;
+    for(int i = 2; i < 1024; ++i){
+        int d = i;
+        for(int j = 2; j * j <= d; ++j) if(d % j == 0){
+            int k = 0;
+            while(d % j == 0){
+                d /= j;
+                ++k;
+            }
+            smallFactors[i].push_back(mp(j,k));
         }
-        for(int x : factorization) printf("%d ",x);
+        if(i <= 41 && smallFactors[i].empty()){
+            MillerRabinTest::witness.push_back(i);
+        }
+        if(d > 1) smallFactors[i].push_back(mp(d,1));
     }
-
+    for(int p = 2; p <= 41; ++p) if(smallFactors[p].size() == 1 && smallFactors[p].back().second == 1) MillerRabinTest::witness.push_back(p);
+    u128 x;
+    while(readInt(x)){
+        factorize(x);
+        refine(factors);
+        for(auto pp : factors){
+            cout << printInt(pp.first) << "^"<<pp.second<<" ";
+        }
+        cout << "\n";
+    }
 }
-
 int main(){
-    
-    FOR(i,1,tcases) testcase();
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
 
+    #ifndef ONLINE_JUDGE
+        freopen("a.inp","r",stdin);
+        freopen("a.out","w",stdout);
+    #else
+        // online submission
+    #endif
+
+    // main program here
+
+    int tcase = 1;
+    //cin >> tcase;
+
+    for(int i = 1; i <= tcase; ++i) testcase();
+
+    // 
+
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    
     return 0;
 }
